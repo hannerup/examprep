c0175c30387e4cec6f224738f2a89e6ab511b60f
hannerup <k.e.hannerup@gmail.com>
2024-12-17 20:21:32 +0100

    mvc
    test 3

---
+++ testasp.net core/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using testasp.net_core.Models;
+
+namespace testasp.net_core.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Opretter menuen med retter og priser
+        private readonly List<Vare> Menu = new List<Vare>
+        {
+            new Vare { ret = "Pasta", pris = 90 },
+            new Vare { ret = "Pizza", pris = 70 },
+            new Vare { ret = "Ris", pris = 50 }
+        };
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Menu = Menu; // Sender menuen til view
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string valgtRet, int alder)
+        {
+            ViewBag.Menu = Menu;
+
+            // Finder den valgte ret
+            Vare? valgtVare = Menu.FirstOrDefault(v => v.ret == valgtRet);
+            if (valgtVare != null)
+            {
+                int pris = valgtVare.GetPris(alder);
+                ViewBag.PrisAtBetale = pris;
+                ViewBag.ValgtRet = valgtRet;
+            }
+            return View();
+        }
+    }
+}
---
+++ testasp.net core/Models/VareModel.cs
@@ -0,0 +1,27 @@
+﻿namespace testasp.net_core.Models
+{
+    public class Vare
+    {
+        public int pris {  get; set; }
+        public string ret { get; set; } = String.Empty;
+
+        public int GetPris(int alder)
+        {
+            int prisAtBetale = pris;
+            
+            if (alder < 12) // Hvis rolling
+            {
+                prisAtBetale = prisAtBetale / 2;
+            }
+            else if (alder >= 65) // Eller olding
+            {
+                prisAtBetale = (int)((float)prisAtBetale * (float)0.80);
+            }
+
+            return prisAtBetale;
+        }
+
+    }
+
+
+}
